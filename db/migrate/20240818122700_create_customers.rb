d441867ff88909946459b2d8c95463b1221c29fe
Mateo Avantaggiato <mate.qac@gmail.com>
2024-08-19 13:31:31 +0200

    core: Added migrations

---
+++ db/migrate/20240818122700_create_customers.rb
@@ -0,0 +1,13 @@
+class CreateCustomers < ActiveRecord::Migration[7.2]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :surname
+      t.string :photo
+      t.references :created_by, null: false, foreign_key: { to_table: :users }
+      t.references :last_modified_by, foreign_key: { to_table: :users }
+
+      t.timestamps
+    end
+  end
+end
